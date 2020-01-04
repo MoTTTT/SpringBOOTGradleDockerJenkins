a2ca6e66a23bc8ab0c31820c923d4194b30b26df
Martin Colley <martinjcolley@gmail.com>
2020-01-05 00:29:48 +0200

    Addedd logger import

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -2,6 +2,8 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.logging.Logger;
+
 
 @RestController
 public class HelloController {
